e0657bad2c58333e0203932ab12a62e641f0da88
francis kamande <franciskamande2001@gmail.com>
2022-02-12 23:36:19 +0300

    Setip complete

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {Brand,CTA,Navbar} from './Components';
+import {Blog,Features,Footer,Header,Possibility,WhatGPT3} from './Containers';
+import './App.css'
+
+const App = () => {
+    return(
+        <div className = "App">
+            <div className = "gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+    )
+} 
+export default App;
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './Article/Article';
+export {default as CTA} from './CTA/CTA';
+export {default as Feature} from './Feature/Feature';
+export {default as Navbar} from './Navbar/Navbar';
+export {default as Brand} from './Brand/Brand';
