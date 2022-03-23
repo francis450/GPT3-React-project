0e72ee4a6223dfa8dc084319c1ee9776956a45d2
francis kamande <franciskamande2001@gmail.com>
2022-03-23 22:12:28 +0300

    The navbar component architecture

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState , setToggleMenu} from 'react';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './Navbar.css';
 
+const Menu = () => (
+		<> 
+		<p><a href = "#Home">Home</a></p>
+        <p><a href = "#wgpt3">What is GPT3</a></p>
+        <p><a href = "#possibility">Open AI</a></p>
+        <p><a href = "#features">Case Studies</a></p>
+        <p><a href = "#blog">Library</a></p>
+        </>
+)
+
 const Navbar = () => {
+		const{toggleMenu, setToggleMenu} = useState(false);
+
     return(
-        <div>
-            Navbar
+        <div className = "gpt3__navbar">
+            <div className = "gpt3__navbar-links">
+                <div className = "gpt3__navbar-links_logo">
+                    <img src = {logo} alt = "logo" />
+                </div>
+                <div className = "gpt3__navbar-links_container">
+                	<Menu />
+                </div>
+            </div>
+            <div className = "gpt3__navbar-sign">
+            	<p>Sign in</p>
+            	<button type = "button">Sign up</button>
+            </div>
+            <div className = "gpt3__navbar-menu">
+            	{toggleMenu
+            		? <RiCloseLine color = "#fff" size={27} onClick = {() => useState(false) } />
+            		: <RiMenu3Line color = "#fff" size={27} onClick = {() => useState(true) } />
+            	}
+            	{toggleMenu && (
+            		<div className = "gpt3__navbar-menu_container scale-up-center">
+            			<div className = "gpt3__navbar-menu_container-links">
+            				<Menu />
+            			</div>
+            			<div className = "gpt3__navbar-menu_container-links-sign">
+            				<p>Sign in</p>
+            				<button type = "button">Sign up</button>
+            			</div>
+            		</div>
+            	)}
+            </div> 
         </div>
     )
 }
 export default Navbar
+//BEM -> Block Element Modifier-> css naming convention used in this project
